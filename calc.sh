208d7e8c5e1f8abd25631825e61aad216f58c59e
Ashwin Ramkumar <8831182+ashwinr63@users.noreply.github.com>
2021-03-18 20:53:13 -0400

    Updating the shell scripting file

--- calc.sh
+++ calc.sh
@@ -9,3 +9,7 @@
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+
+echo "Simple Calculator"
+
+while []
